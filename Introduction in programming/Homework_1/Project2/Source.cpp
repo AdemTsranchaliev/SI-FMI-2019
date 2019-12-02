7a046e48a185cd7ff813d5727483edf3ce93ab01
Adem <ademcran4aliev@abv.bg>
2019-12-02 16:07:23 +0200

    .

--- Introduction in programming/Homework_1/Project2/Source.cpp
+++ Introduction in programming/Homework_1/Project2/Source.cpp
@@ -1,3 +1,15 @@
+/**
+*
+* Solution to homework assignment 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2019/2020
+*
+* @author Adem Tsranchaliev
+* @task 3
+* @compiler VC
+*
+*/
 #include <iostream>
 
 using namespace std;
@@ -8,8 +20,8 @@ void ConvertToHexAndPrint(int);
 int main()
 {
 	char command, firstHexNumber, secondHexNumber;
-	cout << "Enter command, first digit and second digit"<<endl;
-	
+	cout << "Enter command, first hex digit and second hex digit" << endl;
+
 
 	while (true)
 	{
@@ -17,12 +29,12 @@ int main()
 		bool check1 = 1;
 		bool check2 = 1;
 		bool check3 = 1;
-		if (!(command == '+'|| command == '-'|| command == '*'|| command == '/'|| command == '%'))
+		if (!(command == '+' || command == '-' || command == '*' || command == '/' || command == '%'))
 		{
 			check1 = 0;
 		}
 
-		if (!(firstHexNumber >='0'&& firstHexNumber<='9'|| (firstHexNumber >= 'A'&& firstHexNumber <= 'F')))
+		if (!(firstHexNumber >= '0'&& firstHexNumber <= '9' || (firstHexNumber >= 'A'&& firstHexNumber <= 'F')))
 		{
 			check2 = 0;
 		}
@@ -34,27 +46,27 @@ int main()
 
 		if (!(check1&&check2&&check3))
 		{
-			cout << "Invalid input please try again:" << endl << "Enter command, first digit and second digit" << endl;
+			cout << "Invalid input please try again:" << endl << "Enter command, first hex digit and second hex digit" << endl;
 
 		}
 		else
 		{
 			break;
 		}
-		
-		
+
+
 	}
 
 	int firstNum = 0;
 	int secondNum = 0;
-	if (firstHexNumber>=48&&firstHexNumber<=59)
+	if (firstHexNumber >= 48 && firstHexNumber <= 59)
 	{
 		firstNum = firstHexNumber - '0';
 
 	}
 	else if (firstHexNumber >= 65 && firstHexNumber <= 70)
 	{
-		firstNum = (firstHexNumber - 'A')+10;
+		firstNum = (firstHexNumber - 'A') + 10;
 	}
 	if (secondHexNumber >= 48 && secondHexNumber <= 59)
 	{
@@ -68,7 +80,7 @@ int main()
 
 	int result = 0;
 
-	if (command=='+')
+	if (command == '+')
 	{
 		result = firstNum + secondNum;
 	}
@@ -88,36 +100,43 @@ int main()
 	{
 		result = firstNum % secondNum;
 	}
-	
-	ConvertToHexAndPrint(result);
+	if (result == 0)
+	{
+		cout << 0;
+	}
+	else
+	{
+		ConvertToHexAndPrint(result);
+	}
+	cout << endl;
 	system("PAUSE");
 
 }
 
 void ConvertToHexAndPrint(int result)
 {
-	if (result==0)
+	if (result == 0)
 	{
 		return;
 	}
 	ConvertToHexAndPrint(abs(result / 16));
 	int res = abs(result % 16);
-	if (result<=0)
+	if (result <= 0)
 	{
 		cout << "-";
 	}
-	if (res>=0&&res<=9)
+	if (res >= 0 && res <= 9)
 	{
 		char temp = '0';
 		cout << (char)(temp + res);
 	}
-	else if (res>=10&&res<=15)
+	else if (res >= 10 && res <= 15)
 	{
 		char temp = 'A';
-		cout << (char)(temp + (res-10));
+		cout << (char)(temp + (res - 10));
 	}
 
-	
+
 
 }
 
