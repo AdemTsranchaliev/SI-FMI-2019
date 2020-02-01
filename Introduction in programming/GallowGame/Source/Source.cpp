9cd6d9ba880407603f9be8934f72e49e4cdd4c75
Adem <ademcran4aliev@abv.bg>
2020-02-01 11:22:46 +0200

    Finish GALLOW GAME

--- Introduction in programming/GallowGame/Source/Source.cpp
+++ Introduction in programming/GallowGame/Source/Source.cpp
@@ -7,57 +7,62 @@ struct Word
 {
 	char word[8];
 	int count;
+	bool isUsed = false;
 };
+char GetAndCheckLetter();
 void PrintDashes(int,char);
 void AddingWords();
 void NewGame();
-void GameSettings();
+int CheckIfLetterExist(char*,char);
+int GetAndCheckInput(int,int);
 Word words[10];
 int chances = 3;
 int wordCount = 5;
 
 int main()
 {
+	cout << "Wellcome in GALLOW GAME"<<endl;
 	AddingWords();
 	while (true)
 	{
 		int command = 0;
+
+		cout << "MAIN MENU"<<endl<<endl;
+
 		cout << "Press '1' for starting new game " << endl;
 		cout << "Press '2' for settings " << endl;
 		cout << "Press '3' for exit " << endl;
-		cin >> command;
+		command = GetAndCheckInput(1,3);
 
 		if (command==1)
 		{
-
 			NewGame();
-			
 		}
 		else if (command == 2)
 		{
 			cout << "Settings"<<endl;
-			PrintDashes(20,'-');
+	
 			int settingCommand = 0;
 
 			cout << "Press '1' for changing the word count"<<endl;
 			cout << "Press '2' for changing chances count" << endl;
-			cin >> settingCommand;
+			settingCommand = GetAndCheckInput(1,2);
 
 			if (settingCommand==1)
 			{
 				cout << "The current word count is "<<wordCount<<endl;
-				cout << "Enter how much you want to make the word cout(word count must be between 3 and 7)"<<endl;
-				cin >> wordCount;
+				cout << "Enter how much you want to make the word cout(word count must be between 4 and 7)"<<endl;
+				wordCount = GetAndCheckInput(4,7);
 
 				cout << "The word count was changed to "<<wordCount<<endl;
 			}
 			else if (settingCommand == 2)
 			{
-				cout << "The current chances count is " << wordCount << endl;
+				cout << "The current chances count is " << chances << endl;
 				cout << "Enter how much you want to make chances cout(chances count must be between 3 and 7)" << endl;
-				cin >> chances;
+				wordCount = GetAndCheckInput(3,7);
 
-				cout << "Chances count was changed to " << wordCount << endl;
+				cout << "Chances count was changed to " << chances << endl;
 			}
 		}
 		else if (command == 3)
@@ -80,10 +85,20 @@ void PrintDashes(int n,char l)
 
 void NewGame()
 {
-	srand(time(NULL));
-	int randNum = rand() % 10;
+	int randNum = 0;
+	while (true)
+	{
+		 srand(time(NULL));
+	     randNum = rand() % 10;
+		 if (words[randNum].isUsed==false)
+		 {
+			 break;
+		 }
+	}
+	words[randNum].isUsed = true;
 	int wordCount = words[randNum].count;
 	char tempChar[8];
+	int total = 0;
 	int chancesForGame = chances;
 	for (int i = 0; i < wordCount; i++)
 	{
@@ -93,14 +108,33 @@ void NewGame()
 
 	while (true)
 	{
-		PrintDashes(20,'=');
-		cout << "The word is: ";
-		cout << tempChar<<endl;
-		PrintDashes(20, '=');
+
+		cout << "Your chances for mistakes are "<< chancesForGame <<endl;
+	
+		cout << "The word is: '";
+		for (int i = 0; i < words[randNum].count; i++)
+		{
+			if (words[randNum].count!=i+1)
+			{
+				cout << tempChar[i]<<" ";
+			}
+			else
+			{
+				cout << tempChar[i];
+			}
+		}
+		cout << "'"<<endl;
 
 		char letter;
 		cout << "Enter letter which you think can contain the word" << endl;
-		cin >> letter;
+		letter = GetAndCheckLetter();
+		int checkLetterExist = CheckIfLetterExist(tempChar, letter);
+
+		if (checkLetterExist==1)
+		{
+			cout << "The letter '" << letter << "' already is entered" << endl;
+			continue;
+		}
 
 		int findedLetters = 0;
 
@@ -114,26 +148,110 @@ void NewGame()
 		}
 		if (findedLetters == 0)
 		{
-			PrintDashes(30, '-');
+		
 			cout << "The letter '" << letter << "' don't exist in the given word"<<endl;
 			chancesForGame--;
 			if (chancesForGame==0)
 			{
-				cout << "GAME OVER! You couldn't guess the word!"<<endl;
+				cout << "GAME OVER!Your don't have more chances! You couldn't guess the word!"<<endl;
 				cout << "The word is '"<< words[randNum].word<<"'"<<endl;
-				PrintDashes(30, '-');
+				PrintDashes(30, '*');
 				break;
 			}
-			PrintDashes(30, '-');
+		
 		}
 		else
 		{
-			PrintDashes(30, '-');
-			cout << "The word contains the letter '" << letter << "' " << findedLetters << " times" << endl;
-			PrintDashes(30, '-');
+			
+				cout << "The word contains the letter '" << letter << "' " << findedLetters << " times" << endl;
+				total += findedLetters;
+			
 		}
+
+
+		if (total== words[randNum].count)
+		{
+			cout << "You won that game! You guessed the word!"<<endl;
+			cout << "The word is: "<< words[randNum].word << endl;
+			break;
+		}
+
 	}
 }
+int CheckIfLetterExist(char* arr,char letter)
+{
+	int counter = 0;
+	while (arr[counter]!='\0')
+	{
+		if (arr[counter]==letter)
+		{
+			return 1;
+		}
+		counter++;
+	}
+
+	return 0;
+}
+
+int GetAndCheckInput(int start,int end)
+{
+	int input = 0;
+
+	cin >> input;
+
+	while (true)
+	{
+		if (cin.fail())
+		{
+			cout << "Wrong input! Try again!" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin >> input;
+		}
+		else if (input >= start && input <= end)
+		{
+			break;
+		}
+		else
+		{
+			cout << "Wrong input! Try again!" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin >> input;
+		}
+	}
+
+
+	return input;
+}
+
+char GetAndCheckLetter()
+{
+	char input ;
+
+	cin >> input;
+
+	while (true)
+	{
+		if (input>='a'&&input<='z')
+		{
+			break;
+			
+		}
+		else
+		{
+			cout << "Wrong input! Try again!" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin >> input;
+		}
+
+	}
+
+
+	return input;
+}
+
 void AddingWords()
 {
 	words[0] = { "dog",3 };
