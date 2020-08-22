c85b4a48213d8ce44764286fcaa169879af938a8
AdemTsranchaliev <ademcran4aliev@abv.bg>
2020-08-22 10:24:57 +0300

    RAndom

---
+++ Introduction in programming/RANDOM/Random/FindShortest/FindShortest.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void findShortest(char[], char[]);
+bool checkIfSetContainsSign(char,char[]);
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+bool checkIfSetContainsSing(char sign, char set[])
+{
+	int i = 0;
+	while (set[i]!='\0')
+	{
+		if (sign==set[i])
+		{
+			return true;
+		}
+		i++;
+	}
+
+	return false;
+}
+
+
+void findShortest(char mainString[], char charSet[])
+{
+
+	while (true)
+	{
+		while (true)
+		{
+
+		}
+	}
+}
